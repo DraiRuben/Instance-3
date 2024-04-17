92244d9c40d32abbc1d11b0a432d5f56f29da3df
wighthood <zefires@gmail.com>
2024-04-17 16:12:36 +0200

    fish movment
    made two splines for fish movment one for the bugged state and one for the fixed state

---
+++ Assets/Scripts/Minigames/FishingGame/FishBehavior.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class FishBehavior : MonoBehaviour
+{
+    [SerializeField] private SplineContainer[] _Spline;
+    [SerializeField] private float _Speed;
+    [SerializeField] private bool _IsBugged;
+    private float _DistPercent = 0;
+    private float _SplineLength;
+    private int _BugValue;
+    
+
+    private void Start()
+    {
+        if (_IsBugged)
+        {
+            _BugValue = 1;
+        }
+        else
+        {
+            _BugValue = 0;
+        }
+        _SplineLength = _Spline[_BugValue].CalculateLength();
+    }
+
+    private void Update()
+    {
+
+        _DistPercent += _Speed * Time.deltaTime / _SplineLength;
+
+        Vector3 currentPosition = _Spline[_BugValue].EvaluatePosition(_DistPercent);
+        transform.position = currentPosition;
+
+        if (_DistPercent > 1f)
+        {
+            _DistPercent = 0f;
+        }
+    }
+}

d90b942a6bcdf130b83607a2d841ba2c7598dcf3
wighthood <zefires@gmail.com>
2024-04-19 13:59:47 +0200

    displayed score and timer

--- Assets/Scripts/Minigames/FishingGame/PoleManager.cs
+++ Assets/Scripts/Minigames/FishingGame/PoleManager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Pole : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI _FishingScoreText;
+    [SerializeField] private TextMeshProUGUI _FishingTimerText;
     private bool _Fishing;
     private int _FishingScore;
+    private float _FishingTimer;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -18,6 +22,17 @@ public class Pole : MonoBehaviour
                 _Fishing = false;
                 Destroy(other.gameObject);
             }
+        }
+    }
+
+    private void Update()
+    {
+        _FishingTimer += Time.deltaTime;
+        _FishingScoreText.text = "score : " + _FishingScore;
+        _FishingTimerText.text = "time : " + Mathf.RoundToInt(30-_FishingTimer);
+        if (_FishingTimer > 30)
+        {
+            Debug.Log("game end");
         }
     }
     public void Fishing(InputAction.CallbackContext context)

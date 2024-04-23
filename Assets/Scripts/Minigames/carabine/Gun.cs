73f80bbfcd350f2562877cce7c015c6a97d1e7b6
DraiRuben <r.drai@lahorde.school>
2024-04-23 17:22:02 +0200

    Carabine fix, bonneteau anim

--- Assets/Scripts/Minigames/carabine/Gun.cs
+++ Assets/Scripts/Minigames/carabine/Gun.cs
@@ -24,7 +24,11 @@ public class Gun : MonoBehaviour
     [SerializeField] private float _YReach;
     [SerializeField, MinMaxSlider(-10,0)] private Vector2 _GunYRange;
     [System.NonSerialized] private float _MouseYMovementDelta;
-
+    private Sprite _GunSprite;
+    private void Awake()
+    {
+        _GunSprite = GetComponent<SpriteRenderer>().sprite;
+    }
     private void Update()
     {
         _OffsetXTimer += Time.deltaTime / _OffsetXDuration;
@@ -43,15 +47,17 @@ public class Gun : MonoBehaviour
         _PreviousMouseWorldPosition = _WorldMousePosition;
         _MousePosition = Mouse.current.position.ReadValue();
         _WorldMousePosition = Camera.main.ScreenToWorldPoint(_MousePosition);
+
         _MouseYMovementDelta = _WorldMousePosition.y - _PreviousMouseWorldPosition.y;
         _OffsetX = _OffsetXBase * _OffsetXEvolution.Evaluate(_OffsetXTimer);
         _OffsetY = _OffsetYBase * _OffsetYEvolution.Evaluate(_OffsetYTimer);
-        transform.position = new Vector3(_WorldMousePosition.x + _OffsetX, _WorldMousePosition.y - transform.lossyScale.y / 2 - _YReach + _OffsetY);
+        //if scale y is not 1, we need to multiply bounds.y by lossyscale.y
+        transform.position = new Vector3(_WorldMousePosition.x + _OffsetX, _WorldMousePosition.y - _GunSprite.bounds.size.y/ 2 - _YReach + _OffsetY);
 
         //constrains cursor in the Y axis to the shootable zone
         var screenPoint = Mathf.Clamp(_MousePosition.y,
-            Camera.main.WorldToScreenPoint(new(0,transform.parent.position.y+ _GunYRange.x + transform.lossyScale.y / 2 + _YReach)).y , 
-            Camera.main.WorldToScreenPoint(new(0, transform.parent.position.y + _GunYRange.y + transform.lossyScale.y / 2 + _YReach)).y);
+            Camera.main.WorldToScreenPoint(new(0,transform.parent.position.y+ _GunYRange.x + _GunSprite.bounds.size.y / 2 + _YReach)).y , 
+            Camera.main.WorldToScreenPoint(new(0, transform.parent.position.y + _GunYRange.y + _GunSprite.bounds.size.y / 2 + _YReach)).y);
         if(screenPoint!= _MousePosition.y)
         Mouse.current.WarpCursorPosition(new(_MousePosition.x, screenPoint));
     }
--- Assets/Scripts/Systems/Utility.cs
+++ Assets/Scripts/Systems/Utility.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 public enum MedalType
 {
@@ -25,6 +26,13 @@ public interface IInteractable
 
 public static class Utility
 {
+    public static IList<T> Swap<T>(this IList<T> list, int indexA, int indexB)
+    {
+        T tmp = list[indexA];
+        list[indexA] = list[indexB];
+        list[indexB] = tmp;
+        return list;
+    }
     public static Vector3 GetWorldScreenCenterPos()
     {
         Vector2 screenCenterPos = new Vector2(Screen.width/2, Screen.height/2);

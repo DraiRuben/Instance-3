124c6a1b043a5f0d6ce6a622b33bbba0f1a8de83
DraiRuben <r.drai@lahorde.school>
2024-04-24 11:40:00 +0200

    Mole disappearance routine fix & hammer animation fix

--- Assets/Scripts/Minigames/MoleWacker/MoleHammer.cs
+++ Assets/Scripts/Minigames/MoleWacker/MoleHammer.cs
@@ -9,21 +9,31 @@ public class MoleHammer : MonoBehaviour
     private Vector2 _MousePosition;
     private Vector3 _WorldMousePosition;
     private Animator _Animator;
-
+    private float _LastWackTime;
     public void Awake()
     {
         _Animator = GetComponent<Animator>();
     }
-
+    private void OnEnable()
+    {
+        PlayerControls.Instance?.OnSelect.AddListener(() =>
+        {
+            if(Time.time- _LastWackTime > 0.2f)
+            {
+                _LastWackTime = Time.time;
+                _Animator.SetTrigger("Wack");
+            }
+        });
+    }
+    private void OnDisable()
+    {
+        PlayerControls.Instance?.OnSelect.RemoveAllListeners();
+    }
     public void Update()
     {
         _MousePosition = Mouse.current.position.ReadValue();
         _WorldMousePosition = Camera.main.ScreenToWorldPoint(_MousePosition);
         transform.position = new Vector3(_WorldMousePosition.x, _WorldMousePosition.y,0);
         Cursor.visible = false;
-        if (Input.GetMouseButtonDown(0))
-        {
-            _Animator.SetTrigger("Wack");
-        } 
     }
 }

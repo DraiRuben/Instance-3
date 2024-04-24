64b2162d457902bbde7b11c156c279065b26f2f0
DraiRuben <r.drai@lahorde.school>
2024-04-24 10:48:37 +0200

    select input

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
-    FadeInOut _FadeInOut;
     public static PlayerControls Instance;
     [SerializeField] private float _Speed;
     private Rigidbody2D _RBody;
@@ -12,6 +12,7 @@ public class PlayerControls : MonoBehaviour
     private Vector2 _MoveInput;
     private DialogueTrigger _CurrentDialogue;
     private StandInteractableTrigger _CurrentInteractable;
+    [System.NonSerialized] public UnityEvent OnSelect = new(); 
     [System.NonSerialized] public PlayerInput _PlayerInput;
     private void Awake()
     {
@@ -23,6 +24,29 @@ public class PlayerControls : MonoBehaviour
         _Animator = GetComponent<Animator>();
         _SpriteRenderer = GetComponent<SpriteRenderer>();
     }
+    private void FixedUpdate()
+    {
+        _RBody.AddForce(_MoveInput * _Speed * Time.fixedDeltaTime);
+        _RBody.velocity = Vector2.ClampMagnitude(_RBody.velocity, 50);
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var interactable = collision.gameObject.GetComponent<StandInteractableTrigger>();
+        if (interactable)
+        {
+            _CurrentInteractable = interactable;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        var interactable = collision.gameObject.GetComponent<StandInteractableTrigger>();
+        if (_CurrentInteractable == interactable)
+        {
+            _CurrentInteractable = null;
+        }
+    }
+    //overworld action map
     public void Movement(InputAction.CallbackContext context)
     {
         _MoveInput = context.ReadValue<Vector2>();
@@ -53,29 +77,6 @@ public class PlayerControls : MonoBehaviour
             PauseMenu.instance.gameObject.SetActive(Time.timeScale==0);
         }
     }
-
-    private void FixedUpdate()
-    {
-        _RBody.AddForce(_MoveInput * _Speed * Time.fixedDeltaTime);
-        _RBody.velocity = Vector2.ClampMagnitude(_RBody.velocity, 50);
-        
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        var interactable = collision.gameObject.GetComponent<StandInteractableTrigger>();
-        if (interactable)
-        {
-            _CurrentInteractable = interactable;
-        }
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        var interactable = collision.gameObject.GetComponent<StandInteractableTrigger>();
-        if(_CurrentInteractable == interactable)
-        {
-            _CurrentInteractable = null;
-        }
-    }
     public void Interact(InputAction.CallbackContext context)
     {
         if (context.started)
@@ -97,5 +98,11 @@ public class PlayerControls : MonoBehaviour
         {
             _CurrentDialogue.SkipDialogue();
         }
+    }
+    //menu action map
+    public void Select(InputAction.CallbackContext context)
+    {
+        if(context.started)
+            OnSelect.Invoke();
     }
  }

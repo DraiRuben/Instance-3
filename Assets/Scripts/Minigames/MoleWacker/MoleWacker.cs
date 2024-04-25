c1add3ef955083da3e8b0d1ed00ecf8cd61c1af6
DraiRuben <r.drai@lahorde.school>
2024-04-25 11:52:33 +0200

    Fish marre & QR code to imgur instead

--- Assets/Scripts/Minigames/MoleWacker/MoleWacker.cs
+++ Assets/Scripts/Minigames/MoleWacker/MoleWacker.cs
@@ -170,7 +170,7 @@ public class MoleWacker : Minigame
                     GameObject _BugMsg = Instantiate(_BugMessagePrefab);
                     _BugMsg.transform.GetChild(3).GetComponent<TextMeshProUGUI>().SetText("Le sprite de la taupe est introuvable, un fichier de rapport d'erreur a été enregistré dans le dossier du jeu.");
                 }, 1.0f);
-                this.Invoke(() => Application.Quit(), 2.5f);
+                this.Invoke(() => Application.Quit(), 7.0f);
             }
         }
     }

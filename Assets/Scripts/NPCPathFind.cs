1730d81687f53cbba0ddc63874b9be923b941a78
DraiRuben <r.drai@lahorde.school>
2024-05-02 13:48:39 +0200

    Npc merge

--- Assets/Scripts/NPCPathFind.cs
+++ Assets/Scripts/NPCPathFind.cs
@@ -1,15 +1,18 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using UnityEngine;
 
 public class NPCPathFind : MonoBehaviour
 {
     [SerializeField] private GameObject[] _Node;
-    [SerializeField] private float _Duration;
+    private float _MovementSpeed;
     private float _ElapsedTime;
     private int _RandomNode;
-
+    private float _PathLength;
     private Vector3 _StartNode;
     private Vector3 _EndNode;
+    [SerializeField, MinMaxSlider(0,10)] private Vector2 _WaitTime;
+    [SerializeField, MinMaxSlider(0,10)] private Vector2 _Speed;
 
     private void Start()
     {
@@ -17,33 +20,37 @@ public class NPCPathFind : MonoBehaviour
         _StartNode = _Node[_RandomNode].transform.position;
         transform.position = _StartNode;
         _EndNode = _StartNode;
+        StartCoroutine(CheckDistance());
     }
     private void Update()
     {
-        StartCoroutine(CheckDistance());
-        LearpTo();
+        LerpTo();
     }
 
     public IEnumerator CheckDistance()
     {
-        if (Vector3.Distance(transform.position, _EndNode) <= 0.001f)
+        while (true)
         {
-            _ElapsedTime = 0.0f;
-            _Duration = Random.Range(2f, 4f);
-            _StartNode = _EndNode;
-
-            while (_EndNode == _StartNode)
+            if (Vector3.Distance(transform.position, _EndNode) <= 0.001f)
             {
-                _EndNode = _Node[Random.Range(0, _Node.Length)].transform.position;
+                _ElapsedTime = 0.0f;
+                _MovementSpeed = Random.Range(_Speed.x, _Speed.y);
+                _StartNode = _EndNode;
+                while (_EndNode == _StartNode)
+                {
+                    _EndNode = _Node[Random.Range(0, _Node.Length)].transform.position;
+                }
+                _PathLength = Vector3.Distance(transform.position, _EndNode);
+                yield return new WaitForSeconds(Random.Range(_WaitTime.x, _WaitTime.y));
             }
+            yield return new WaitForFixedUpdate();
         }
-        yield return new WaitForFixedUpdate();
+       
     }
 
-    private void LearpTo()
+    private void LerpTo()
     {
-        _ElapsedTime += Time.deltaTime;
-        float _PercentageComplete = _ElapsedTime / _Duration;
-        transform.position = Vector3.Lerp(_StartNode, _EndNode, Mathf.SmoothStep(0, 1, _PercentageComplete));
+        _ElapsedTime += Time.deltaTime* _MovementSpeed /  _PathLength;
+        transform.position = Vector3.Lerp(_StartNode, _EndNode, Mathf.SmoothStep(0, 1, _ElapsedTime));
     }
 }
